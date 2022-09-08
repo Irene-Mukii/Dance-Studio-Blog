376a336eeab96c95cf730ea35485252c855f30a4
renniequin@gmail.com <renniequin@gmail.com>
2022-09-08 16:54:04 +0300

    allows header to remain visible even when scrolling

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,17 @@
-//Select elements
+//Select elements --reed documentation on query selector
 const selectElement = selector =>{
     const element = document.querySelector(selector);
     if(element) return element;
     throw new Error(`Something went wrong, makesure that ${selector} exists or is typed correctly.`);
-}
+};
+
+//Nav styles on scroll
+const scrollHeader = ()=> {
+    const headerElement= selectElement('#header');
+    if(this.scrollY>=15){
+        headerElement.classList.add('activated');
+    }else { 
+        headerElement.classList.remove('activated');
+    }
+};
+// window.addEventListener('scroll', scrollHeader); //test for header change upon scroll
